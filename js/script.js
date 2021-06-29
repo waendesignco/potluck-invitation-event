5c645dec4bfb1a963a5fcfeed6bc04a8b93f18b1
ryoko-y <wagarden6@gmail.com>
2021-06-29 16:34:06 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var darkModeButton = document.querySelector(".dark-mode");
+var theme = document.querySelector("body");
+darkModeButton.addEventListener("click", function () {
+  if (theme.classList.contains("dark-palette")) {
+    theme.classList.remove("dark-palette");
+    darkModeButton.innerText = "Dark Mode";
+  } else {
+    theme.classList.add("dark-palette");
+    darkModeButton.innerText = "Light Mode";
+  }
+});
